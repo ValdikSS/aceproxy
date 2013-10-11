e2eca55679f8434383deb037fef5a9e3843a476a
ValdikSS <iam@valdikss.org.ru>
2013-10-11 21:56:01 +0300

    Remote key generator timeout

--- aceclient/acemessages.py
+++ aceclient/acemessages.py
@@ -48,7 +48,7 @@ class AceMessage(object):
 	  hashlib.sha1(request_key + product_key).hexdigest()
       else:
 	# Use site with key generator
-	return 'READY key=' + urllib2.urlopen("http://valdikss.org.ru/tv/key.php?key=" + request_key).read()
+	return 'READY key=' + urllib2.urlopen("http://valdikss.org.ru/tv/key.php?key=" + request_key, timeout = 4).read()
     # End READY_KEY
     
     @staticmethod
