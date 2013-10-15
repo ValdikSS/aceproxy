a4c35bcd3f22e8ec2ea4083015837a6039863218
ValdikSS <iam@valdikss.org.ru>
2013-10-15 22:24:06 +0300

    Raketa-tv.com plugin

---
+++ plugins/raketatv_config.py
@@ -0,0 +1,9 @@
+'''
+Raketa-tv.com Playlist Downloader Plugin configuration file
+'''
+
+# Insert your Raketa-tv.com playlist URL here
+url = 'http://raketa-tv.com/player/JSON/channels_authorized_list.json'
+# Host override. Use this if you're behind NAT.
+# Example: '1.2.3.4'
+host = False
---
+++ plugins/raketatv_plugin.py
@@ -0,0 +1,65 @@
+'''
+Raketa-tv.com Playlist Downloader Plugin
+Original code by tohm
+http://ip:port/raketatv
+'''
+import re
+import logging
+import urllib2
+import time
+import json
+from base64 import b64decode
+from PluginInterface import AceProxyPlugin
+import raketatv_config
+
+
+class Raketatv(AceProxyPlugin):
+    handlers = ('raketatv', )
+
+    logger = logging.getLogger('plugin_raketatv')
+    url = raketatv_config.url
+    host = raketatv_config.host
+    playlist = None
+    playlisttime = None
+
+    def downloadPlaylist(self):
+        try:
+            Raketatv.logger.debug('Trying to download playlist')
+            playlist = urllib2.urlopen(Raketatv.url, timeout=10).read()
+            playlisttime = int(time.time())
+        except:
+            Raketatv.logger.error("Can't download playlist!")
+            return False
+
+        try:
+            jsonplaylist = json.loads(playlist)['channels']
+            playlist = "#EXTM3U\n"
+            for channel in jsonplaylist:
+                title = channel['title'].encode('utf-8')
+                pid = channel['id'].replace('|', 'M').replace('?', 'L')
+                pid = b64decode(pid)
+                playlist += '#EXTINF:-1,' + title + "\n" + pid + "\n"
+
+            Raketatv.playlist = playlist
+            Raketatv.playlisttime = playlisttime
+        except Exception as e:
+            Raketatv.logger.error("Can't parse playlist! " + repr(e))
+            return False
+
+        return True
+
+    def handle(self, connection):
+        if not Raketatv.playlist or (int(time.time()) - Raketatv.playlisttime > 60 * 60):
+            if not self.downloadPlaylist():
+                connection.dieWithError()
+                return
+
+        if Raketatv.host:
+            hostport = Raketatv.host + ':' + str(connection.request.getsockname()[1])
+        else:
+            hostport = connection.request.getsockname()[0] + ':' + str(connection.request.getsockname()[1])
+
+        connection.send_response(200)
+        connection.send_header('Content-type', 'application/x-mpegurl')
+        connection.end_headers()
+        connection.wfile.write(re.sub('([0-9a-f]{40})', 'http://' + hostport + '/pid/\\1', Raketatv.playlist))
