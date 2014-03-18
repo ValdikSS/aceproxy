3cabd551c0d0c11c39051ecea6301b5a1d476f59
ValdikSS <iam@valdikss.org.ru>
2014-03-18 09:52:31 +0400

    Raketa-tv.com plugin removed

--- plugins/raketatv_config.py
+++
@@ -1,9 +0,0 @@
-'''
-Raketa-tv.com Playlist Downloader Plugin configuration file
-'''
-
-# Insert your Raketa-tv.com playlist URL here
-url = 'http://raketa-tv.com/player/JSON/channels_list.json'
-# Host override. Use this if you're behind NAT.
-# Example: '1.2.3.4'
-host = False
